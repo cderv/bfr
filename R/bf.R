7d0cbd2291def69a7723e3d9fd22b41ad87706e3
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-10-07 12:11:06 +0200

    add a function to get previsions data

---
+++ R/bf.R
@@ -0,0 +1,34 @@
+json_prevision <- "http://www.bison-fute.gouv.fr/previsions/previsions.json"
+
+only_departements <- seq_len(94)
+
+get_prevision_json <- function(json = json_prevision) {
+  jsonlite::fromJSON(json_prevision)
+}
+
+
+#' Get all data available at the current date
+#'
+#' bison fute website uses data from past date and future date
+#' for its previsions color of departement. See http://www.bison-fute.gouv.fr/previsions,10564.html
+#'
+#' @return a tibble
+#'
+#' @importFrom tibble tibble
+#' @importFrom tidyr unnest
+#' @importFrom purrr map modify_depth simplify_all
+get_all_previsions_available <- function() {
+  res <- get_prevision_json(json)
+  prev_bf <- tibble::tibble(
+    days = res$days,
+    depts = list(res$deptsLine[only_departements]),
+    depart = purrr::map(res$values, ~strsplit(.x[only_departements], split = ",")) %>%
+      purrr::modify_depth(2, 1) %>%
+      purrr::simplify_all(),
+    retour = purrr::map(res$values, ~strsplit(.x[only_departements], split = ",")) %>%
+      purrr::modify_depth(2, 2) %>%
+      purrr::simplify_all()
+  ) %>%
+    tidyr::unnest()
+  prev_bf
+}
